0628232fe73efb2506cea4faf5105afc67322334
mh.bakhshipour <mh.bakhshipour@yahoo.com>
2019-12-05 03:25:30 +0330

    Add feature search

--- core/serializer.py
+++ core/serializer.py
@@ -1,21 +1,33 @@
-from django.db import transaction
 from rest_framework import serializers
+
 from core.models import Category, ContactUs, Faq
 
 
-class CreateContactUsSerializer(serializers.ModelSerializer):
+class ContactUsSerializer(serializers.ModelSerializer):
     class Meta:
         model = ContactUs
         fields = ['id', 'full_name', 'email', 'phone', 'description', 'c_created_at', 'status']
 
 
-class GetFaqSerializer(serializers.ModelSerializer):
+class FaqSerializer(serializers.ModelSerializer):
     class Meta:
         model = Faq
         fields = ['id', 'title', 'description', 'c_created_at']
 
 
-class GetCategoriesSerializer(serializers.ModelSerializer):
+class CategoriesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id', 'title', 'slug', 'parent']
+
+
+class SearchSerializer(serializers.Serializer):
+
+    def to_representation(self, instance):
+        return instance
+
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
--- core/views.py
+++ core/views.py
@@ -1,30 +1,58 @@
+from django.db.models import Q
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, filters
 
-from core.serializer import GetCategoriesSerializer
+from blog.models import News
+from consultant.models import Consultant
+from core.serializer import SearchSerializer, ContactUsSerializer, FaqSerializer, CategoriesSerializer
 from core.models import Faq, Category
-from core.serializer import CreateContactUsSerializer, GetFaqSerializer
 from mehrgan.custom_view_mixins import ExpressiveCreateContactUsViewSetModelMixin, ExpressiveListModelMixin
 
 
-class CreateContactUsViewSet(ExpressiveCreateContactUsViewSetModelMixin, generics.CreateAPIView):
-    serializer_class = CreateContactUsSerializer
+class ContactUsViewSet(ExpressiveCreateContactUsViewSetModelMixin, generics.CreateAPIView):
+    serializer_class = ContactUsSerializer
     singular_name = 'contact_us_form_created'
 
 
-class GetFaqViewSet(ExpressiveListModelMixin, generics.ListAPIView):
-    serializer_class = GetFaqSerializer
-    plural_name = 'faqs'
+class FaqViewSet(ExpressiveListModelMixin, generics.ListAPIView):
+    serializer_class = FaqSerializer
+    plural_name = 'faqs_list'
+    search_fields = ['title', 'description']
+    filter_backends = (filters.SearchFilter,)
 
     def get_queryset(self):
         queryset = Faq.objects.all().order_by('-created_at')
         return queryset
 
 
-class GetCategoriesViewSet(ExpressiveListModelMixin, generics.ListAPIView):
-    serializer_class = GetCategoriesSerializer
-    plural_name = 'categories'
+class CategoriesViewSet(ExpressiveListModelMixin, generics.ListAPIView):
+    serializer_class = CategoriesSerializer
+    plural_name = 'categories_list'
 
     def get_queryset(self):
         queryset = Category.objects.get_all_categories()
+        return queryset
+
+
+class SearchViewSet(ExpressiveListModelMixin, generics.ListAPIView):
+    serializer_class = SearchSerializer
+    plural_name = 'results'
+
+    def get_queryset(self):
+        query = self.request.GET.get('query')
+        t_d_lookup = (Q(title__icontains=query) | Q(description__icontains=query))
+        t_d_c_lookup = (Q(title__icontains=query) | Q(description__icontains=query) | Q(content__icontains=query))
+        n_a_a_lookup = (Q(full_name__icontains=query) | Q(activity__icontains=query) | Q(about__icontains=query))
+
+        news = {'news': list(
+            News.objects.filter(t_d_c_lookup).distinct().values('id', 'title', 'description', 'thumbnail', 'slug'))}
+        categories = {'categories': list(
+            Category.objects.filter(t_d_lookup).distinct().values('id', 'title', 'description', 'thumbnail', 'slug',
+                                                                  'parent'))}
+        consultant = {'consultant': list(
+            Consultant.objects.filter(n_a_a_lookup).distinct().values('id', 'full_name', 'about', 'avatar',
+                                                                      'phone_number', 'activity'))}
+        faqs = {'faqs': list(Faq.objects.filter(t_d_lookup).distinct().values('id', 'title', 'description'))}
+
+        queryset = [news, categories, consultant, faqs]
         return queryset
