c83cb776d1e2e9624b1d6a91032a1ecde851f9c2
mh.bakhshipour <mh.bakhshipour@yahoo.com>
2020-01-06 15:59:03 +0330

    Add translate missing

--- consultant/apps.py
+++ consultant/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext as _
 
 
 class ConsultantConfig(AppConfig):
     name = 'consultant'
+    verbose_name = _("consultant")
