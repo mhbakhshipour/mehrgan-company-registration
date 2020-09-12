7ca8416f661824799bf2bc6f4e9e89033ade3e1b
mh.bakhshipour <mh.bakhshipour@yahoo.com>
2020-09-12 23:04:38 +0430

    Dockerize Project

--- manage.py
+++ manage.py
@@ -3,12 +3,9 @@
 import os
 import sys
 
-from dotenv import load_dotenv
 
-load_dotenv(dotenv_path='./mehrgan/{}'.format(os.environ.get('environment')), verbose=True)
-
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mehrgan.settings")
+def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mehrgan.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -18,3 +15,7 @@ if __name__ == "__main__":
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
